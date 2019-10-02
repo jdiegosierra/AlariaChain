91ab2fa0c4840680b04d81f3da7b59ab9f24b7d9
Diego <diego@pop-os.localdomain>
2019-10-03 00:13:27 +0200

    created blockchain object

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,7 +1,12 @@
 // Dependencies
 use std::time::{SystemTime, UNIX_EPOCH};
+use sha2::{
+    digest::generic_array::{typenum::U32, GenericArray},
+    Digest, Sha256,
+};
+// use sha2::{Sha256, Digest}; // Digest permite que se pueda hacer el new() por que?
 
-pub fn getTimestamp() -> u128 {
+pub fn get_timestamp() -> u128 {
     let tmp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
         .unwrap(); // Leer Error Handling
@@ -9,6 +14,21 @@ pub fn getTimestamp() -> u128 {
     tmp.as_secs() as u128
 }
 
-// fn setHash () {
+// pub fn show_hash ()  {
+//     // create a Sha256 object
+//     let mut hasher = Sha256::new();
+//     hasher.input(b"hholilil");
+//     let result = hasher.result();
+//     println!("{:x}", result) // con {} daba fallo, por qué?
+//     // result.to_vec()
 
 // }
+
+
+
+pub fn get_hash(data: &String) -> GenericArray<u8, U32> {
+    let mut hasher = Sha256::new();
+    hasher.input(data);
+    let result = hasher.result();
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,79 @@
 // modules
 mod lib;
 
+use sha2::{
+    digest::generic_array::{typenum::U32, GenericArray},
+    Digest, Sha256,
+};
+
 #[derive(Debug)]
+#[derive(Clone)]
 // Creamos estructura publica de bloque, para que pueda ser accesible desde los padres.
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
-    pub data: [String; 1], // TODO: Aumentar número de transacciones. Usar bytes.
+    pub data: String, // TODO: Aumentar número de transacciones. Usar bytes.
     pub prev: [String; 1],
-    pub hash: [String; 1]
+    pub hash: GenericArray<u8, U32>
+}
+
+pub struct Blockchain {
+    pub blocks: Vec<Block>
+}
+
+impl Block {
+    fn new(index: u32, timestamp: u128, data: String, prev: [String; 1]) -> Self {
+
+        let tmp = &data; // se puede hacer sin esto?
+
+        Block {
+            index,
+            timestamp,
+            data: tmp.clone(),
+            prev,
+            hash: get_hash(tmp)
+        }
+    }
+
+    // fn get_hash(mut data: [String; 1]) -> GenericArray<u8, U32> {
+    //     let mut hasher = Sha256::new();
+    //     hasher.input(&mut data[0]);
+    //     let result = hasher.result();
+    //     result
+    // }
+}
+
+// Función que obtiene el hash de un bloque
+pub fn get_hash(data: &String) -> GenericArray<u8, U32> {
+    let mut hasher = Sha256::new();
+    hasher.input(&data);
+    let result = hasher.result();
+    result
+}
+
+impl Blockchain {
+    fn new() -> Self {
+        Blockchain {
+            blocks: vec![]
+        }
+    }
+
+    fn add_block(&mut self, block: Block) {
+        self.blocks.push(block.clone());
+    }
 }
 
 fn main() {
+    
     println!("Welcome to AlariaChain!");
 
-    let block1 = Block {
-        index: 1,
-        timestamp: lib::getTimestamp(),
-        data: [String::from("Alice => Bob 1BTC")],
-        prev: [String::from("previous hash")],
-        hash: [String::from("hash of this block")]
-    };
+    println!("Creating genesis Block...");
+    let genesisblock = Block::new(1, lib::get_timestamp(), String::from("This is the genesis block"), [String::from("00000000")]);
 
-    println!("El primer bloque es: ");
-    println!("{:#?}", block1);
+    // println!("El primer bloque es: ");
+    // println!("{:#?}", genesisblock);
+
+    let mut blockchain = Blockchain::new();
+    println!("Adding genesis Block...");
+    blockchain.add_block(genesisblock);
 }
